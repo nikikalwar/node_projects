300c23d5e4a79d50241dd7479bd366edb80ec493
nikikalwar <nikikumar27@gmail.com>
2020-09-11 16:13:41 +0530

    parsing request methods

--- section3/myapp/index.js
+++ section3/myapp/index.js
@@ -9,9 +9,21 @@ Primary file for the api
 //Dependencies
 
 var http=require('http');
+var url=require('url');
 //the server should respond to all the requests with a string
 var server=http.createServer(function(req,res){
-    res.end("Hello world\n");
+
+    //get the URL and parse it
+    var parsedUrl=url.parse(req.url,true);
+
+    //get the path
+        var path=parsedUrl.pathname;
+        var trimmedPath=path.replace(/^\/+|\/+$/g,'');
+    //send the response
+    res.end("hello World\n");
+
+    //log the request path
+        console.log('Request received on path: '+trimmedPath);
 });
 //start the server,and have it listen to port 3000
 server.listen(3000,function(){
