cd3132915e6877cf843fd336c5a0703382de54cd
nikikalwar <nikikumar27@gmail.com>
2020-09-16 23:55:48 +0530

    parsing payloads

--- section3/myapp/index.js
+++ section3/myapp/index.js
@@ -7,9 +7,9 @@ Primary file for the api
 */
 
 //Dependencies
-
 var http=require('http');
 var url=require('url');
+var StringDecoder=require('string_decoder').StringDecoder;
 //the server should respond to all the requests with a string
 var server=http.createServer(function(req,res){
 
@@ -26,18 +26,30 @@ var server=http.createServer(function(req,res){
         var method=req.method.toLowerCase();
         //get the headers which we are receiving
         var headers=req.headers;
+        
+        //get the payload, if any
+        var decoder=new StringDecoder('utf-8');
+        var buffer='';
+        req.on('data',function(data){
+            buffer+=decoder.write(data);
+        });
+            req.on('data',function(data){
+                buffer+=decoder.end();
+            });
+                
+            //printing the payload which we received
+            console.log('Request received with payload ',buffer);
+            //send the response
+           // res.end("hello World\n");
 
-    //send the response
-    res.end("hello World\n");
+            //log the request path
+            // console.log('Request received on path: '+trimmedPath+ ' with this method--> '+ method+' the query parameter is --> ',queryStringObject);
 
-    //log the request path
-       // console.log('Request received on path: '+trimmedPath+ ' with this method--> '+ method+' the query parameter is --> ',queryStringObject);
-
-        //log the headers
-        console.log("the header is ",headers);
+                //log the headers
+          //      console.log("the header is ",headers);
 
     });
-//start the server,and have it listen to port 3000
-server.listen(3000,function(){
-    console.log("the server is listening to port 3000 now");
-});
+            //start the server,and have it listen to port 3000
+            server.listen(3000,function(){
+                console.log("the server is listening to port 3000 now");
+            });
