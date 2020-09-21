52a2e3ae76021d9a16b7a8ce169061a8530b0a1e
nikikalwar <nikikumar27@gmail.com>
2020-09-21 23:30:19 +0530

    parsing request paths

--- restapi/app.js
+++ restapi/app.js
@@ -4,13 +4,31 @@ Primary files
 
 //Dependencies
 var http=require('http');
-
+var url=require('url');
 
 //the server responds to all request with a string
 var server=http.createServer(function(req,res){
-    res.end('Hello World\n');
+    
+
+    //get the url and parse it
+    var parsedUrl=url.parse(req.url,true);
+
+    
+    //get the path 
+    var path=parsedUrl.pathname;
+    var trimmedPath=path.replace(/^\/+|\/+$/g,'');
+ 
+    //log the path
+    console.log('Request received on the path:'+trimmedPath)
+    //send the response
+    res.end('Hello to the port 3000');
+
+
+
 
 });
+
+
 
 //start the server, and have it listen on port 3000
 
